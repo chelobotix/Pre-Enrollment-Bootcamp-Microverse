261b1cb22816f799a0783304d944e544c872e872
Marcelo Alarcon <marceloalarconbarrenechea@gmail.com>
2022-11-07 14:07:05 -0400

    added bon-appetit.js

---
+++ bon-appetit.js
@@ -0,0 +1,24 @@
+function bonAppetit(bill, k, b) {
+      // Write your code here
+      // Write your code here
+      let total = 0;
+      for (let i = 0; i < bill.length; i++) {
+            if (i != k) {
+                  total = total + bill[i];
+
+            }
+      }
+
+      total = total / 2;
+
+      if (total - b === 0) {
+            console.log("Bon Appetit");
+      }
+      else {
+            console.log(Math.abs(total - b));
+      }
+
+
+}
+array = [3, 10, 2, 9]
+bonAppetit(array, 1, 12)
